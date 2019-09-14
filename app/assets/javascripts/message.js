63b4257f994703de9b62bc7a521a46078d0d3135
adatu0527 <adatu0527@yahoo.ne.jp>
2019-09-14 16:46:53 +0900

    非同期通信　実装

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,68 @@
+
+
+ $(function(){
+   
+  
+   
+
+  function buildMessage(message){
+    var img = message.image ? `<img src= ${ message.image } > ` : "";
+    var html = `<div class="message-box">
+                  <div class="message-box__top-box">
+                    <div class="message-box__top-box__user-box">
+                      ${message.name}
+                    </div>
+                    <div class="message-box__top-box__time-box">
+                      ${message.time}
+                    </div>
+                  </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                        ${img}
+                    </div>
+                </div>`
+
+    return html;
+
+  }
+  
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+    var fd = new FormData(this);
+    var url = window.location.href +'';
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: fd,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      
+      var html = buildMessage(message);
+      $('.messages').append(html)
+      
+      $('#message_content').val("")
+      $('.hidden').val("")
+      
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
+    })
+    .fail(function(){
+      alert('エラー')
+
+     })
+    .always(() => {
+      $(".form__submit").removeAttr("disabled");
+    });
+
+
+  
+  });
+});
+
+
+
+
