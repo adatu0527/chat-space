d64738452d2b7e9b6308b1b80d64084bda2f250c
adatu0527 <adatu0527@yahoo.ne.jp>
2019-09-22 21:25:07 +0900

    trub link 実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -20,69 +20,72 @@ $(function(){
                 </div>`
     return html;
   }
-  $('#new_message').on('submit',function(e){
-    e.preventDefault();
-    var fd = new FormData(this);
-    var url = window.location.href +'';
-    console.log(this)
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: fd,
-      dataType: "json",
-      processData: false,
-      contentType: false
-    })
-    .done(function(message){
-      var html = buildMessage(message);
-      $('.messages').append(html)
-      $('#new_message')[0].reset();
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
+  $(document).on('turbolinks:load', function() { 
+    $('#new_message').on('submit',function(e){
       
-    })
-    .fail(function(){
-      alert('エラー')
 
-     })
-    .always(() => {
-      $(".form__submit").removeAttr("disabled");
-    });
+      e.preventDefault();
+      var fd = new FormData(this);
+      var url = window.location.href +'';
+      console.log(this)
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: fd,
+        dataType: "json",
+        processData: false,
+        contentType: false
+      })
+      .done(function(message){
+        var html = buildMessage(message);
+        $('.messages').append(html)
+        $('#new_message')[0].reset();
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
+        
+      })
+      .fail(function(){
+        alert('エラー')
 
-    
-  });
-
-  function reloadMessages()  {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-    
-    last_message_id = $('.message-box__top-box').last().data('id');
-    console.log(last_message_id)
-    $.ajax({
-      url: 'api/messages',
-      type: 'GET',
-      dataType: 'json',
-      data: {id: last_message_id }
-    })
-    .done(function(messages){
-      console.log(messages)
-      var insertHTML = '';
-      messages.forEach(function (message) {
-        insertHTML = buildMessage(message);
-      $('.messages').append(insertHTML);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      
-      
+      })
+      .always(() => {
+        $(".form__submit").removeAttr("disabled");
       });
-      
 
-    })
-    .fail(function() {
-      alert('自動更新に失敗しました');
       
     });
-  }
-};
+
+    function reloadMessages()  {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      
+      last_message_id = $('.message-box__top-box').last().data('id');
+      console.log(last_message_id)
+      $.ajax({
+        url: 'api/messages',
+        type: 'GET',
+        dataType: 'json',
+        data: {id: last_message_id }
+      })
+      .done(function(messages){
+        console.log(messages)
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildMessage(message);
+        $('.messages').append(insertHTML);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        
+        
+        });
+        
+
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+        
+      });
+    }
+  };
   
   setInterval(reloadMessages, 5000);
    
   });
-
+});
--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -16,73 +16,74 @@ $(function(){
                 </div>`
               return html;
   }
-   
-    $('#user-search-field').on("keyup",function(e){
-      e.preventDefault(); 
-      var input = $("#user-search-field").val();
-    
-      $.ajax({
-        url: '/users/search', 
-        type: 'GET', 
-        data: { keyword: input }, 
-        dataType: 'json' 
-      })
-      .done(function(users){ 
-        
-        $('.user-search-result').empty();
-        
-        if (users.length !== 0) {
-          users.forEach(function(user){
-            var html = buildMember(user);
-            $('.user-search-result').append(html);
-            
-          });
-        }
-        else {
-          var errormessage= "一致するユーザーはいません"
-          console.log("ok");
-          buildHtml(errormessage);
-          $('.user-search-result').append(errormessage);
-        };
-      
-      })
-      .fail(function() {
-        alert('error');
-      });
-    });
-});
-//追加
-$(function(){
-  function buildEntermember(hogename,hogeid) {
-  var html = `<div class='chat-group-user'>
-                <input name='group[user_ids][]' type='hidden' value=${hogeid}>
-                <p class='chat-group-user__name'>${hogename}</p>
-                <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
-              </div>`
-  return html;
-  }
-  
-  $('.user-search-result').on('click' ,".user-search-add" , function(e){
-    console.log("ok");
-    e.preventDefault();
-    
-    $(this).parent().remove();
-    var hoge = this
-    var hogename =$(hoge).data('name')
-    console.log(hogename);
-    var hogeid =$(hoge).data('id')
-    console.log(hogeid);
-    var html = buildEntermember(hogename,hogeid);
-    $('.user-toadd-member').append(html);
-  });
-  
-});
-//削除
-$(function(){
-  $('.user-toadd-member').on('click' ,".user-search-remove" , function(e){
-    e.preventDefault();
+  $(document).on('turbolinks:load', function() {
 
-    $(this).parent().remove();
+      $('#user-search-field').on("keyup",function(e){
+        e.preventDefault(); 
+        var input = $("#user-search-field").val();
+      
+        $.ajax({
+          url: '/users/search', 
+          type: 'GET', 
+          data: { keyword: input }, 
+          dataType: 'json' 
+        })
+        .done(function(users){ 
+          
+          $('.user-search-result').empty();
+          
+          if (users.length !== 0) {
+            users.forEach(function(user){
+              var html = buildMember(user);
+              $('.user-search-result').append(html);
+              
+            });
+          }
+          else {
+            var errormessage= "一致するユーザーはいません"
+            console.log("ok");
+            buildHtml(errormessage);
+            $('.user-search-result').append(errormessage);
+          };
+        
+        })
+        .fail(function() {
+          alert('error');
+        });
+      });
+  });
+  //追加
+  $(function(){
+    function buildEntermember(hogename,hogeid) {
+    var html = `<div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value=${hogeid}>
+                  <p class='chat-group-user__name'>${hogename}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+    return html;
+    }
+    
+    $('.user-search-result').on('click' ,".user-search-add" , function(e){
+      console.log("ok");
+      e.preventDefault();
+      
+      $(this).parent().remove();
+      var hoge = this
+      var hogename =$(hoge).data('name')
+      console.log(hogename);
+      var hogeid =$(hoge).data('id')
+      console.log(hogeid);
+      var html = buildEntermember(hogename,hogeid);
+      $('.user-toadd-member').append(html);
+    });
+    
+  });
+  //削除
+  $(function(){
+    $('.user-toadd-member').on('click' ,".user-search-remove" , function(e){
+      e.preventDefault();
+
+      $(this).parent().remove();
+    });
   });
 });
- 
