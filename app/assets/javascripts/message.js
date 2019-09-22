241bff7f3f59ca7f51beb4ce916bff9af396e59e
adatu0527 <adatu0527@yahoo.ne.jp>
2019-09-22 17:48:52 +0900

    自動更新　実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
 
     var img = message.image ? `<img src= ${ message.image } > ` : "";
     var html = `<div class="message-box">
-                  <div class="message-box__top-box">
+                  <div class="message-box__top-box" data-id=${message.id}>
                     <div class="message-box__top-box__user-box">
                       ${message.name}
                     </div>
@@ -24,6 +24,7 @@ $(function(){
     e.preventDefault();
     var fd = new FormData(this);
     var url = window.location.href +'';
+    console.log(this)
     $.ajax({
       url: url,
       type: 'POST',
@@ -47,4 +48,33 @@ $(function(){
       $(".form__submit").removeAttr("disabled");
     });
   });
-});
+
+  function reloadMessages()  {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+    last_message_id = $('.message-box__top-box').last().data('id');
+    console.log(last_message_id)
+    $.ajax({
+      url: 'api/messages',
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_message_id }
+    })
+    .done(function(messages){
+      var insertHTML = '';
+      messages.forEach(function (message) {
+        insertHTML = buildMessage(message);
+      $('.messages').append(insertHTML);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      });
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
+      
+    });
+  }
+};
+  
+  setInterval(reloadMessages, 5000);
+   
+  });
+
